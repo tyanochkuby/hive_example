16fd702236c1a3214a250acda5e006ac10278029
tyanochku_by <ramyud@st.amu.edu.pl>
2023-09-13 21:22:01 +0300

    hive setup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+
   runApp(const MainApp());
 }
 
